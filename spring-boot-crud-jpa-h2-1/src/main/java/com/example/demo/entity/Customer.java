84baa576ceaf430eced42106397363931ec5cf16
jitendragupta10 <103560975+jitendragupta10@users.noreply.github.com>
2023-02-23 00:12:25 +0530

    Add files via upload

---
+++ spring-boot-crud-jpa-h2-1/src/main/java/com/example/demo/entity/Customer.java
@@ -0,0 +1,110 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Table(name="tbl_customer")
+@Entity
+@Setter
+@Getter
+@ToString
+public class Customer {
+	
+	
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Customer(Long id, String name, Long age, String location, Date createdAt, Date updatedAt) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.location = location;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getAge() {
+		return age;
+	}
+
+	public void setAge(Long age) {
+		this.age = age;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private Long age;
+	
+	private String location;
+	
+	@CreationTimestamp
+	@Column(name="created_at", nullable=false, updatable=false)
+	private Date createdAt;
+	
+	@UpdateTimestamp
+	@Column(name="updated_at")
+	private Date updatedAt;
+	
+}
+
+
